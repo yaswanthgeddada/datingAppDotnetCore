6c42d9438e974a7554db906af07a63f453dcf08d
yaswanthgeddada <yaswanthgeddada@gmail.com>
2021-02-18 16:23:42 +0530

    init

---
+++ API/-- SQLite.sql
@@ -0,0 +1,11 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1,"James");
+
+
+INSERT INTO Users (Id, UserName)
+VALUES (2,"Tommy");
+
+
+INSERT INTO Users (Id, UserName)
+VALUES (3,"facak");
